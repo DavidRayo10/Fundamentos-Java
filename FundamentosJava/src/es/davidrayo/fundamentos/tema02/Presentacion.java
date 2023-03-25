3269321f59b5faa71d8bec2227ee5012db20be77
David Rayo <davidrayo.desarrollador@gmail.com>
2023-03-25 12:03:45 +0100

    presentación: método main

---
+++ FundamentosJava/src/es/davidrayo/fundamentos/tema02/Presentacion.java
@@ -0,0 +1,9 @@
+package es.davidrayo.fundamentos.tema02;
+
+public class Presentacion {
+	public static void main(String[] args) {
+		System.out.println("Hola, soy David Rayo.");
+		System.out.println("¡Bienvenido al repositorio de \"Fundamentos de Java\"!");
+	}
+
+}
