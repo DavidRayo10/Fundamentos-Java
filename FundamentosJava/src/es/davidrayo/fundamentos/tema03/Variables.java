b8d64bb04407fad85930ff35f5927bf0c0f8d475
David Rayo <davidrayo.desarrollador@gmail.com>
2023-03-25 12:16:57 +0100

    tema 03: tipos de variables

---
+++ FundamentosJava/src/es/davidrayo/fundamentos/tema03/Variables.java
@@ -0,0 +1,24 @@
+package es.davidrayo.fundamentos.tema03;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		/// número enteros
+		byte numero1 = 10;
+		short numero2 = 12;
+		int numero3 = 125;
+		long numero4 = 243;
+
+		// números decimales
+		float numero5 = 12.5f;
+		double numero6 = 32.4;
+
+		// variables textuales
+		char letra = 'A';
+		String texto = "David";
+
+		// variable booleana
+		boolean isMayor = true;
+
+	}
+}
